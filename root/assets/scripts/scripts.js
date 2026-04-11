35fca8547fe34a2742aaabf559848db21e1df7a9
Ezri King <flowingtribe@gmail.com>
2026-04-10 23:31:50 -0500

    Updated script,js and styles.css + a few other things Updated Technical Issues doc to .md instead of nothing Added deferred script to dashboard, accounts, account-detail, and transactions html pages. Added 'let' before script.js variables. Made navigation links, header, and footer spawn from script.js Began adding :root colors to scripts.css Organized nav-headermain-footer in container ids and flexboxes to match wireframe layout. Added some more TODOs.

--- root/assets/scripts/scripts.js
+++ root/assets/scripts/scripts.js
@@ -1,7 +1,17 @@
+/* TODO: 
+    Add header, footer, and nav, then convert them to imports and exports in separate files.
+
+    Add sandwich toggle 3 lines stacked thing, then also adjust sandwich toggle for media query
+*/
+
 "use strict";
 
-header = document.getElementsByTagName('header')[0];
-footer = document.getElementsByTagName('footer')[0];
+let header = document.getElementsByTagName('header')[0];
+let footer = document.getElementsByTagName('footer')[0];
+let nav = document.getElementsByTagName('nav')[0];
+let pageTitle = document.getElementsByTagName('title')[0].textContent;
+
+let dashboardLink = 
 
 // const title = "Hello World! This is a Slug";
 // const slug = title
@@ -16,24 +26,31 @@ window.addEventListener('load', function() {
     /* add head to document 
         link stylesheet(s)
         link script(s)
-        set meta tags
     */
     this.document.head.innerHTML += `
-        <link rel="stylesheet" href="assets/css/styles.css">
+        <link rel="stylesheet" href="../assets/css/styles.css">
     `;
 
-    // /* add class to body */
-    // document.body.classList.add('custom-style');
+    /* add header to document */
+    header.innerHTML = `
+        <h1>${pageTitle}</h1>
+    `;
 
-    // /* add style to head */
-    // var style = document.createElement('style');
-    // style.innerHTML = `
-    //     .custom-style {
-    //         background-color: #f0f0f0;
-    //         color: #333;
-    //         padding: 10px;
-    //         border-radius: 5px;
-    //     }
-    // `;
-    // document.head.appendChild(style); */
+    /* add navigation to document */
+    nav.innerHTML = `
+        <!-- Figure out how to do that three-lines-stacked toggle -->
+        <!-- Nav links are currently relative to any file inside the pages directory -->
+        <!-- If I decide to make navigation at the top of the page, I can re-add <span class="desktop">&nbsp;</span> after each link -->
+        <!-- Nav should be in a vertical column on the left side of the page -->
+        <nav class="nav-toggle">
+            <a href="dashboard.html">Dashboard</a>
+            <a href="transactions.html">Transactions</a>
+			<a href="accounts.html">Accounts</a>
+		</nav>
+    `;
+
+    /* add footer to document */
+    footer.innerHTML = `
+        <p>&copy; 2026 Income and Expense Tracker by Ezri King</p>
+    `;
 });

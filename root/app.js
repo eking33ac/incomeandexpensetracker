5748677cacd6950dfd200ae687e153045e9f3fe9
Ezri King <flowingtribe@gmail.com>
2026-04-25 17:36:47 -0500

    Installed packages and added npm start to package.json
    installed expressjs and ejs for project
    
    installed nodemon for dev so project server auto-restarts on save
    
    added npm start to package.json scripts
    
    added the very start of express app creation to app.js

--- root/app.js
+++ root/app.js
@@ -1,8 +1,28 @@
-/* Entry point for Node.js server. Sets up server, initializes middleware, and mounts routes. (not yet implemented) */
+/* Entry point for Node.js server. Sets up server, initializes middleware, and mounts routes. */
+
+/* imports */
+const express = require('express');
+
+/* project's imports */
+
+// create app
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Connect to MySQL database using mysql2 package
-
-const mysql = require('mysql2');
+// To be implemented in when database connection is needed. For now, data is stored in json files for development purposes.
+/* const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -14,4 +34,4 @@ const connection = mysql.createConnection({
 connection.connect(err => {
   if (err) throw err;
   console.log('✅ Connected to MySQL!');
-});
+}); */
